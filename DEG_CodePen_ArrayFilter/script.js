9ddb88be74b8eba427073fd35c343d48694cb4f4
Aaron <agoldman12@gmail.com>
2018-11-01 21:52:11 -0500

    Add files via upload
    Uploaded project files

---
+++ DEG_CodePen_ArrayFilter/script.js
@@ -0,0 +1,37 @@
+/* 
+Below we have an array of objects. Each object represents a person.
+
+Goal: Learn to use the Array.filter method
+
+Task: Create a new array that contains only people who have alligance to "House Stark" 
+
+Hint: You will need to define a custom filter function
+
+For reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+*/
+
+const people = [
+	{name: 'Robb Stark', allegiance: 'House Stark'},
+	{name: 'Danerys Targaryen', allegiance: 'House Targaryen'},
+	{name: 'Cersei Lannister', allegiance: 'House Lannister'},
+	{name: 'Jon Snow', allegiance: 'House Stark'},
+	{name: 'Lysa Aryn', allegiance: 'House Baelish'},
+	{name: 'Arya Stark', allegiance: 'House Stark'}
+];
+
+// filter people array here
+/*const houseStark = people.filter(function(person){
+		
+		return person.allegiance == 'House Stark';
+});
+*/
+
+const houseStark = people.filter(person => person.allegiance == 'House Stark');
+
+
+function displayNames(people) {
+	return people.map(person => person.name)
+		.join('<br/>');
+}
+
+document.querySelector('div').innerHTML = `${displayNames(houseStark)}`
